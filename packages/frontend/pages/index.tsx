7acfbf4bf723fa1121501e7f1e3c3b935e2298cb
Everton Mourinho <everton.mourinho@hotmail.com>
2023-01-03 15:05:56 +0000

    feat: add auth

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,11 +1,28 @@
-import Link from "next/link"
+import { Button } from '@mui/material'
+import Link from 'next/link'
+import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth'
+import { auth } from '../services/firebase'
+import { useState } from 'react'
 
-export default ()=>{
+export default () => {
+  const [user, setUser] = useState<User>({} as User)
+
+  const onGoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider()
+    const response = await signInWithPopup(auth, provider)
+    setUser(response.user)
+  }
+
   return (
-      <div>
-          <h1>Home</h1>
-          <Link href="/lobby">Partidas Online</Link>
-          <Link href="/admin">Admin</Link>
-      </div>
+    <div>
+      <h1>Hero Chess Battle</h1>
+      <Link href="/lobby">Partidas Online</Link>
+      <Link href="/admin">Admin</Link>
+
+      <h2>{user.displayName}</h2>
+
+      {user.photoURL && <img src={user.photoURL} alt={user.displayName} />}
+      {!user.uid && <Button onClick={onGoogleSignIn}>Entrar com Google</Button>}
+    </div>
   )
 }
